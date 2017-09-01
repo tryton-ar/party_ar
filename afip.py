ed3aa45cc854dbd55c3be2262bbef1a9ca94a75e
Luciano Rossi <lukio@gcoop.coop>
2017-09-01 02:41:09 -0300

    issue #8 Add afip.country model

---
+++ afip.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# This file is part of the party_ar module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+
+from trytond.model import ModelView, ModelSQL, fields
+import logging
+logger = logging.getLogger(__name__)
+
+__all__ = ['AFIPCountry']
+
+
+class AFIPCountry(ModelSQL, ModelView):
+    'AFIP Country'
+    __name__ = 'afip.country'
+
+    code = fields.Char('Code')
+    name = fields.Char('Name')
