3af184f4c85d7e055d8588d14fdf4af7bc71496a
Adrián Bernardi <adrian.bernardi@gmail.com>
2018-12-26 19:17:14 -0300

    Use own function get_cache_dir

--- afip.py
+++ afip.py
@@ -2,10 +2,23 @@
 # This file is part of the party_ar module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+import os
+import logging
 
 from trytond.model import ModelView, ModelSQL, fields
 
+logger = logging.getLogger(__name__)
+
 __all__ = ['AFIPCountry']
+
+
+def get_module_install_dir():
+    basepath = __file__
+    return os.path.dirname(os.path.abspath(basepath))
+
+
+def get_cache_dir():
+    return os.path.join(get_module_install_dir(), 'cache')
 
 
 class AFIPCountry(ModelSQL, ModelView):
