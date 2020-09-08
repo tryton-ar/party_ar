4346f9c6faedb3120a4d92d5d8a12c0fe292166e
Adrián Bernardi <adrian.bernardi@gmail.com>
2020-09-08 11:35:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ import re
 from configparser import ConfigParser
 from setuptools import setup
 
+MODULE = 'party_ar'
+PREFIX = 'trytonar'
 MODULE2PREFIX = {}
 
 
@@ -31,7 +33,7 @@ def get_require_version(name):
 
 
 config = ConfigParser()
-config.read_file(open('tryton.cfg'))
+config.read_file(open(os.path.join(os.path.dirname(__file__), 'tryton.cfg')))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
@@ -40,7 +42,6 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'trytonar_party_ar'
 
 download_url = 'https://github.com/tryton-ar/party_ar/tree/%s.%s' % (
     major_version, minor_version)
@@ -66,20 +67,20 @@ requires.append(get_require_version('pysimplesoap'))
 tests_require = [get_require_version('proteus')]
 dependency_links = list(LINKS.values())
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
     description='Tryton module that integrates AFIP (Argentina) with party',
     long_description=read('README'),
     author='tryton-ar',
     url='https://github.com/tryton-ar/party_ar',
     download_url=download_url,
-    package_dir={'trytond.modules.party_ar': '.'},
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.party_ar',
-        'trytond.modules.party_ar.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.party_ar': (info.get('xml', []) + [
+        'trytond.modules.%s' % MODULE: (info.get('xml', []) + [
             'tryton.cfg', 'view/*.xml', 'locale/*.po', 'data/*.xml']),
         },
     classifiers=[
@@ -89,8 +90,8 @@ setup(name=name,
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later'
-        ' (GPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
@@ -111,8 +112,8 @@ setup(name=name,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    party_ar = trytond.modules.party_ar
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
